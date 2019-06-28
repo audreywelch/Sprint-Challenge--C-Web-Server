240c03e4acd25dd9939fc1d3f49da45f5f565f7d
audreywelch <welch.audreyl@gmail.com>
2019-06-28 09:03:05 -0700

    Parsed the input URL

--- src/client.c
+++ src/client.c
@@ -27,27 +27,51 @@ typedef struct urlinfo_t {
 */
 urlinfo_t *parse_url(char *url)
 {
+  // localhost:3490/d20
   // copy the input URL so as not to mutate the original
-  char *hostname = strdup(url);
-  char *port;
-  char *path;
+  char *hostname = strdup(url); // localhost
+  char *port; // 3490
+  char *path; // d20
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
+    ///////////////////
+    // IMPLEMENT ME! //
+    ///////////////////
+  
+    // We can parse the input URL by doing the following:
 
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    // 1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
+    char *slash;
+    slash = '/';
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+    path = strchr(hostname, *slash + 1);
+    printf("Path, or everything that comes after the /: %s", path);
+
+    // 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+    char *p;
+    p = strstr(hostname, slash); // Finds the slash in url
+    if (p) {
+      strcpy(p, '\0'); // localhost:3490 \0 d20
+      printf("%s", hostname);
+    }
+
+    // 4. Use strchr to find the first colon in the URL.
+    char *colon;
+    colon = ':';
+
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+    port = strchr(hostname, *colon + 1);
+    printf("Port, or everything that comes after the colon: %s", port);
+
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+    char *q;
+    q = strstr(hostname, colon);
+    if (q) {
+      strcpy(q, '\0'); // localhost
+      printf("%s", hostname);
+    }
 
   return urlinfo;
 }
