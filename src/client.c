e4eac17f1fb3660792c7c00341ea6a1370127f4d
audreywelch <welch.audreyl@gmail.com>
2019-06-28 11:02:31 -0700

    MVP met

--- src/client.c
+++ src/client.c
@@ -42,25 +42,25 @@ urlinfo_t *parse_url(char *url)
     // We can parse the input URL by doing the following:
 
     // 1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    char *slash;
-    slash = '/';
-    char *first_backslash = strchr(hostname, slash);
+    // char *slash;
+    // slash = '/';
+    char *first_backslash = strchr(hostname, '/');
 
     // 2. Set the path pointer to 1 character after the spot returned by strchr.
     path = first_backslash + 1;
-    printf("Path, or everything that comes after the /: %s\n", path);
+    printf("Path, or everything that comes after the /, is: %s\n", path);
 
     // 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
     *first_backslash = '\0'; // localhost:3490 \0 d20
 
     // 4. Use strchr to find the first colon in the URL.
-    char *colon;
-    colon = ':';
-    char *first_colon = strchr(hostname, colon);
+    // char *colon;
+    // colon = ':';
+    char *first_colon = strchr(hostname, ':');
 
     // 5. Set the port pointer to 1 character after the spot returned by strchr.
     port = first_colon + 1;
-    printf("Port, or everything that comes after the colon: %s\n", port);
+    printf("Port, or everything that comes after the colon, is: %s\n", port);
 
     // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
     *first_colon = '\0'; // localhost
@@ -108,7 +108,7 @@ int send_request(int fd, char *hostname, char *port, char *path)
   rv = send(fd, request, request_length, 0);
 
   if (rv < 0) {
-    perror("send");
+    perror("error in send_request()");
   }
 
   return rv;
@@ -132,6 +132,7 @@ int main(int argc, char *argv[])
 
   // 1. Parse the input URL
   urlinfo = parse_url(argv[1]); // "localhost:3490/d20"
+  
   // 2. Initialize a socket by calling the `get_socket` function from lib.c
   sockfd = get_socket(urlinfo->hostname, urlinfo->port);
 
